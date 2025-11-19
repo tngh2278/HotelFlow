7015c77297756f9368ef9096effca88197cd1232
SiHyunKm <114273362+SiHyunKm@users.noreply.github.com>
2025-11-19 23:41:45 +0900

    Delete HotelFlow-main directory
    잘못 올린 프로젝트 파일 삭제

--- HotelFlow-main/src/main/java/cse/oop2/hotelflow/Common/model/RoomServiceOrderStatus.java
+++
@@ -1,7 +0,0 @@
-package cse.oop2.hotelflow.Common.model;
-
-public enum RoomServiceOrderStatus {
-    REQUESTED,   // 요청됨
-    COMPLETED,   // 처리 완료
-    CANCELED     // 취소
-}
--- HotelFlow-main/src/main/java/cse/oop2/hotelflow/Common/model/UserRole.java
+++
@@ -1,8 +0,0 @@
-package cse.oop2.hotelflow.Common.model;
-
-public enum UserRole {
-    ADMIN,
-    STAFF,
-    CUSTOMER
-    
-}
