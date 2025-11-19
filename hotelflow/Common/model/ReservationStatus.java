9021ac3789a23ed191e2e76a53a70727de04badf
SiHyunKm <114273362+SiHyunKm@users.noreply.github.com>
2025-11-19 23:46:40 +0900

    Delete hotelflow directory
    잘못 올린 파일 삭제

--- hotelflow/Common/model/ReservationStatus.java
+++
@@ -1,8 +0,0 @@
-package cse.oop2.hotelflow.Common.model;
-
-public enum ReservationStatus {
-    RESERVED,      // 예약 완료 (아직 체크인 전)
-    CHECKED_IN,    // 체크인 완료
-    CHECKED_OUT,   // 체크아웃 완료
-    CANCELED       // 예약 취소
-}
--- hotelflow/Common/model/RoomStatus.java
+++
@@ -1,8 +0,0 @@
-package cse.oop2.hotelflow.Common.model;
-
-public enum RoomStatus {
-    VACANT ,   //빈 방
-    OCCUPIED,  //숙박 중
-    CLEANING, //청소 중
-    MAINTENANCE //점검 중
-}
